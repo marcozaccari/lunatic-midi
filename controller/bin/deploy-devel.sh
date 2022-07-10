b38cae8f6c70c573cea0458d3ec8d43ebe78304b
Marco Zaccari <info@marcozaccari.com>
2022-07-11 00:47:26 +0200

    devel: deploy, build and run.

--- controller/bin/deploy-devel.sh
+++ controller/bin/deploy-devel.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-rsync -av --delete --exclude=.vscode ../controller/ pi@lunatic-devel:/opt/lunatic
+check_errors() {
+	if [ ! $? -eq 0 ]; then
+		echo "Error, see above"
+		exit 1
+	fi
+}
 
-ssh pi@lunatic-devel "cd /opt/lunatic && go build && ./main -config-file=bin/linear_keyboard.json"
+HIGH='\033[0;31m'
+NC='\033[0m'
+
+echo -e "$HIGH Sync sources local => remote $NC\n"
+rsync -av --delete --exclude=.vscode ../ pi@lunatic:/opt/lunatic
+check_errors
+
+echo -e "\n$HIGH Compile $NC\n"
+ssh pi@lunatic ". /etc/profile && cd /opt/lunatic && go build"
+check_errors
+
+. run-devel.sh
